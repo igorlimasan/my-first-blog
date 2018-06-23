abe0aace813071686ea42e8d480a8f01ed719c74
Igor Lima <igorlimadossantos.igorlimasan@gmail.com>
2018-06-23 17:28:15 -0300

    Alteraçao do user

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'wo+_)vpy*=(cy#xkjg!l*2rg5z*$0*^z3!+qu_qg!ks0w(e5$c'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', '<your_username>.pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'igorlimasan.pythonanywhere.com']
 
 
 # Application definition
